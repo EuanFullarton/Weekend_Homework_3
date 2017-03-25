bc382a5b9e8d9e80b03a5d8ab66fc94ce3934f80
Euan Fullarton <euanfullarton7@gmail.com>
2017-03-25 11:52:53 +0000

    Inital setup of required files, basic methods such as .name working in pry

---
+++ seeds.rb
@@ -0,0 +1,17 @@
+require ('pry')
+require_relative ('models/customer')
+require_relative ('models/film')
+require_relative ('models/ticket')
+
+
+customer1 = Customer.new({'name' => 'Percy', 'funds' => 10})
+customer1.save()
+
+film1 = Film.new({'title' => 'Goodfellas', 'price' => 5})
+film1.save()
+
+ticket1 = Ticket.new({"customer_id" => customer1.id, "film_id" => film1.id})
+ticket1.save()
+
+binding.pry
+nil
